a518ea07ca80a2c37c3b5af53242bc85e656e3df
yewon kang <kyeah01@gmail.com>
2021-03-24 14:59:24 +0900

    200324 | 007 | rename submodule dir to content & remove eslint

---
+++ pages/1st_blog/_slug.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="">
+    <v-chip
+      v-if="chip1"
+      class="ma-2"
+      close
+      @click:close="chip1 = false">
+      Closable
+    </v-chip>
+    <article>
+      <nuxt-content :document="article" />
+    </article>
+    <blog>
+      <nuxt-content :document="blog" />
+    </blog>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('', params.slug).fetch()
+    return {
+      article,
+    }
+  },
+}
+</script>
+<style>
+.nuxt-content h2 {
+  font-weight: bold;
+  font-size: 28px;
+}
+.nuxt-content h3 {
+  font-weight: bold;
+  font-size: 22px;
+}
+</style>
